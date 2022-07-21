5b5597c1148d8244ea9eedab706e9ccbe539fab5
Davis Li <2211479+davisli@users.noreply.github.com>
2022-07-21 20:50:05 +0800

    NGINX Install Script without sudo
    Somehow sudo slows down the processes. Could be SELINUX?

---
+++ install_script.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+echo -e "YUM Repo Update\n"
+yum update -y
+echo -e "Install wget to retrieve nginx .crt/.key\n"
+yum install wget -y
+echo -e "make /etc/ssl/nginx directory to store nginx crt/key\n"
+mkdir /etc/ssl/nginx
+cd /etc/ssl/nginx
+echo -e "Getting NGINX-Plus .crt and .key\n"
+sleep 5s
+echo -e "Enter the nginx-repo.crt given to you"
+read repoCrt
+wget $repoCrt
+sleep 5s
+echo -e "Enter the nginx-repo.key given to you"
+read repoKey
+wget $repoKey
+sleep 5s
+cd /etc/ssl/nginx
+echo -e "Output NGINX cert expiry for you to verify\n"
+openssl x509 -enddate -noout -in nginx-repo.crt
+echo -e "Installing ca-certificates, epel-release\n"
+sleep 5s
+yum install ca-certificates -y
+yum install epel-release -y
+echo -e "Retrieving NGINX-Plus Repo with NGINX .crt and .key\n"
+wget -P /etc/yum.repos.d https://cs.nginx.com/static/files/nginx-plus-7.4.repo
+sleep 5s
+echo -e "Installing NGINX-Plus\n"
+yum install nginx-plus -y
+systemctl enable nginx.service
+nginx -v
+sleep 10s
+yum install nginx-plus-module-rtmp -y
+yum install nginx-plus-module-prometheus -y
+sleep 5s
+echo -e "Installed NGINX Dynamic Modules"
+ls /etc/nginx/modules
+echo "Configure your SELinux or set to disabled in /etc/selinux/config"
